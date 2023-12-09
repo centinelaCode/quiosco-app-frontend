9a7b038cf81a4b74baf7bff67e9a26018a4f89a5
centinelaCode <centinela1024tb@gmail.com>
2023-12-09 16:51:38 -0600

    [ADD] finish to router, config two layous with his children routes

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,8 +1,11 @@
+import { Outlet } from 'react-router-dom'
 
 export default function AuthLayout() {
    return (
       <div>
          AuthLayout
+
+         <Outlet />
       </div>
   )
 }
--- src/router.jsx
+++ src/router.jsx
@@ -2,15 +2,34 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import Layout from './layouts/Layout'
 import AuthLayout from './layouts/AuthLayout'
+import Inicio from './views/Inicio'
+import Login from './views/Login'
+import Register from './views/Register'
 
 const router = createBrowserRouter([
    {
       path: '/',
-      element: <Layout />
+      element: <Layout />,
+      children: [
+         {
+            index: true,
+            element: <Inicio />
+         }
+      ]      
    },
    {
       path: '/auth',
-      element: <AuthLayout /> 
+      element: <AuthLayout /> ,
+      children: [
+         {
+            path: '/auth/login',
+            element: <Login />
+         },
+         {
+            path: '/auth/register',
+            element: <Register />
+         }
+      ]
    }
 ])
 
