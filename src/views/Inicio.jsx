3043f4c01b4b1db296c8fe4cce3be9eb9e4149f4
centinelaCode <centinela1024tb@gmail.com>
2023-12-10 00:09:58 -0600

    [ADD] show products in grid, pendient details

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,8 +1,23 @@
+import { products } from '../data/products'
+import Product from '../components/Product'
 
 export default function Inicio() {
+
    return (
-      <div>
-         Inicio
-      </div>
+      <>
+         <h1 className='text-4xl my-5 font-black'>Inicio</h1>
+         <p className='text-2xl my-10'>
+         Elige y personaliza tu pedido con los siguientes productos.
+         </p>
+
+         <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+            {products.map((product) => (
+               <Product 
+                  key={product.imagen}
+                  product={product}
+               />
+            ))}
+         </div>
+      </>
    )
 }
