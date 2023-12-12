0af8f96fd21d6161b34c80f48fe17ee86240fc60
centinelaCode <centinela1024tb@gmail.com>
2023-12-12 16:05:00 -0600

    [ADD] add scroll in products for fixed sidebar and resume, and finish card products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,3 +1,4 @@
+import { formatearDinero } from '../helpers'
 
 export default function Product({product}) {
    const {nombre, imagen, precio} = product
@@ -12,7 +13,14 @@ export default function Product({product}) {
 
          <div className="p-5">
             <h3 className="text-2xl font-bold">{nombre}</h3>
-            <p className="mt-5 font-black text-4xl text-amber-500">{precio}</p>
+            <p className="mt-5 font-black text-4xl text-amber-500">{formatearDinero(precio)}</p>
+
+            <button
+               type='button'
+               className='bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 font-bold rounded-md'
+            >
+               Agregar
+            </button>
          </div>
       </div>
    )
